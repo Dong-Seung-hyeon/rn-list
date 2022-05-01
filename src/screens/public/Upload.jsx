95a125cb89fc59b4f8602d370c38702a5de84879
seunghyeondong <kj94gakt@naver.com>
2022-05-01 21:31:45 +0900

    뒤로가기버튼

---
+++ src/screens/public/Upload.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+//components
+import Header from '../../components/Header'
+
+const Detail = () => {
+  return (
+    <View>
+      <Header title='홈' />
+      <Text> 업로드화면 입니다. </Text>
+    </View>
+  );
+};
+
+export default Detail;
